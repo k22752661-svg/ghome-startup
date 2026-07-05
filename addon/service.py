353a2189cf2fdf79168317a42d530795a70b80b6
k22752661-svg <k22752661@gmail.com>
2026-07-05 16:06:50 +0530

    Create service.py

---
+++ addon/service.py
@@ -0,0 +1,15 @@
+import xbmc
+import xbmcaddon
+
+addon = xbmcaddon.Addon()
+
+xbmc.log("G HOME Startup Service Started", xbmc.LOGINFO)
+
+# Future:
+# - Internet Check
+# - GitHub Config
+# - Welcome Video
+# - Offline Video
+# - Expired Video
+# - Ad Video
+# - Open Favourites
